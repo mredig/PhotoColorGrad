75466b41b268ef9171d67f163ba8c3f1d83a1a32
Michael Redig <mredig@gmail.com>
2020-04-11 23:59:55 -0500

    (feat) added color to hex

--- PhotoColorGrad/Shared/UIColor+Codable.swift
+++ PhotoColorGrad/Shared/UIColor+Codable.swift
@@ -28,5 +28,21 @@ extension UIColor {
 		getRed(&red, green: &green, blue: &blue, alpha: &alpha)
 		return UIColorRep(red: red, green: green, blue: blue, alpha: alpha)
 	}
+
+	@objc var hexString: String {
+		var red: CGFloat = 0
+		var green: CGFloat = 0
+		var blue: CGFloat = 0
+		var alpha: CGFloat = 0
+		getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+		let rInt = UInt32(red * 255)
+		let gInt = UInt32(green * 255)
+		let bInt = UInt32(blue * 255)
+		let aInt = UInt32(alpha * 255)
+		let total = (rInt << 24) | (gInt << 16) | (bInt << 8) | aInt
+
+		return String(total, radix: 16)
+	}
 }
 
